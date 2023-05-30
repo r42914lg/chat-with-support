20ad659534bda14b26f229642b48c61f4a011419
lgdars22 <lgdars22@gmail.com>
2023-05-30 10:23:53 +0300

    build should fail

--- app/src/main/java/com/r42914lg/chatsandbox/MainActivity.kt
+++ app/src/main/java/com/r42914lg/chatsandbox/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ChatSandboxTheme {
+            ChatSandboxTheme___ {
                 Surface(
                     modifier = Modifier
                         .fillMaxSize()
