d3c484329bc82ad708020b0068f01af7de66e179
lgdars22 <lgdars22@gmail.com>
2023-06-01 20:54:51 +0300

    v1.0.12

--- app/src/androidTest/java/com/r42914lg/chatsandbox/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/r42914lg/chatsandbox/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.r42914lg.chatsandbox", appContext.packageName)
+        assertEquals("com.r42914lg.chatsandbox__", appContext.packageName)
     }
 }
